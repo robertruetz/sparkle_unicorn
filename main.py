8e622b6465b12a7023e19014307f74812bca1766
Robert Carrington <Robert.Carrington@q2ebanking.com>
2016-10-02 13:25:18 -0500

    Added CSS and JS routes

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def index_page():
 def send_js(path):
     return send_from_directory('js', path)
 @app.route('/css/<path:path>')
-def send_js(path):
+def send_css(path):
     return send_from_directory('css', path)
 
 def get_typeahead(typeahead):
